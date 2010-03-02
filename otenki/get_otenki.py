6e6b23dce6a3b1ed283cdf4515efa3bd21623d88
Junichi Shinohara <tabe1hands.tech@gmail.com>
2010-03-03 01:08:27 +0900

    updated

--- otenki/get_otenki.py
+++ otenki/get_otenki.py
@@ -9,22 +9,21 @@ import BeautifulSoup
 import html5lib
 
 class Otenki(object):
-
     def __init__(self):
         self._uri = self.get_uri()
-        self._soup = self.get_soup(self._uri)
-
-    def get_uri(self):
-        _today = self.get_today()
-        _url="http://rd.yahoo.co.jp/rss/l/weather/days/*http://weather.yahoo.co.jp/weather/jp/13/4410.html?d=" + _today
-        return _url
+        self._soup = self.get_soup_dom(self._uri)
 
     def get_today(self):
         _tm = time.localtime()
         _today = "%(year)d%(month)02d%(day)02d" % {"year":_tm.tm_year, "month":_tm.tm_mon, "day":_tm.tm_mday}
         return _today
 
-    def get_soup(self, uri):
+    def get_uri(self):
+        _today = self.get_today()
+        _url="http://rd.yahoo.co.jp/rss/l/weather/days/*http://weather.yahoo.co.jp/weather/jp/13/4410.html?d=" + _today
+        return _url
+
+    def get_soup_dom(self, uri):
         source = urllib.urlopen(uri).read()
         try:
             # BeautifulSoupでパース
@@ -35,26 +34,31 @@ class Otenki(object):
             soup = parser.parse(source)
         return soup
 
-    def get_otenki(self):
-        _td_comment = self.get_today_comment()
-        _tm_comment = self.get_today_comment()
-        #import pdb;pdb.set_trace()
-        print _td_comment.findNext()
-        print _tm_comment.findNext()
-        return _td_comment, _tm_comment
-
     def get_comment(self, text):
         _comment = self._soup.find(text=re.compile(text))
         return _comment
 
-    def get_today_comment(self):
+    def get_today_dom(self):
         _comment = self.get_comment("Today")
-        return _comment
+        return _comment.findNextSiblings()[2]
 
-    def get_tomorrow_comment(self):
+    def get_tomorrow_dom(self):
         _comment = self.get_comment("Tomorrow")
-        return _comment
+        return _comment.findNextSiblings()[1]
+
+    def print_otenki(self):
+        _td_dom = self.get_today_dom()
+        _tm_dom = self.get_tomorrow_dom()
+        print self.get_plain_text(_td_dom)
+        #print self.get_plain_text(_tm_dom)
+        return True
+
+    def get_plain_text(self, dom):
+        text = ''.join([ s.string if s.string else self.get_plain_text(s) for s in dom ])
+        ctug = re.compile('<!--.*?-->',re.DOTALL)
+        text = ctug.sub('', text)
+        return text
 
 if __name__ == '__main__':
     o = Otenki()
-    print o.get_otenki()
+    o.print_otenki()
