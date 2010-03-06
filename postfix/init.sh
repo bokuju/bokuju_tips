06a72f3363081ae298bc4f599d7da99c71260b6b
root <root@moco.bokuju.com>
2010-03-06 13:58:43 +0900

    updated

--- postfix/init.sh
+++ postfix/init.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
-if [ -f $(pwd $0)/common.sh ]; then
-    source $(pwd init.sh)/common.sh
+###
+# Including
+#
+if [ -f $(pwd $0)/.postfix.sh ]; then
+    source $(pwd $0)/.postfix.sh
 fi
 
+###
+# Settings
+#
 ret=0
+domainname="$(get_domainname $(hostname))"
 
+###
+# Main
+#
 backup ${backup_files}
 
-set_key_eq_value "${main_cf}" 'myhostname' "mail.$(get_domainname $(hostname))"
-set_key_eq_value "${main_cf}" 'mydomain' "$(get_domainname $(hostname))"
-set_key_eq_value "${main_cf}" 'myorigin' "$(get_key_eq_value "${main_cf}" 'myhostname')"
+# 基本設定
+set_main_cf 'myhostname' "mail.${domainname}"
+new_myhostname="$(get_main_cf 'myhostname')"
+
+set_main_cf 'mydomain' "${domainname}"
+new_mydomain="$(get_main_cf 'mydomain')"
+
+set_main_cf 'myorigin' "${new_myhostname}"
+
+set_main_cf 'inet_interfaces' 'all'
+
+set_main_cf 'mydestination' "${new_myhostname}, localhost.${new_mydomain}, localhost, ${new_mydomain}"
+
+set_main_cf 'home_mailbox' 'Maildir\/'
+
+set_main_cf 'smtpd_banner' "${new_myhostname} ESMTP unknown"
+
+# SMTP-Auth
+
+# 受信メールサイズ制限
 
 if [ ${ret} -eq 0 ]; then
     remove_backup ${backup_files} 
